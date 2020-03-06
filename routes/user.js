15fe064f0b95531d1c96364e347b788a02761e09
Marko Zlatar <markozlatar01@gmail.com>
2020-03-06 20:21:42 +0100

    ADDED: Get user param validation

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body, param } = require("express-validator");
 
 const User = require("../models/user");
 
@@ -9,6 +10,22 @@ const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 // GET => /users/:username
-router.get("/:username", userControllers.getUser);
+router.get(
+  "/:username",
+  [
+    param("username")
+      .isString()
+      .isLength({ min: 2, max: 32 })
+      .custom(value => {
+        const regEx = /^[\w\.\-\_]{2,32}$/;
+
+        if (!regEx.test(value)) throw new Error("Character validation failed");
+
+        return true;
+      })
+      .trim()
+  ],
+  userControllers.getUser
+);
 
 module.exports = router;
