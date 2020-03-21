3ca1d2c07dfb1b77b7c83fa4eb68e19cf4d2820a
Marko Zlatar <markozlatar01@gmail.com>
2020-03-21 20:05:44 +0100

    CREATED: Basic private chat

--- controllers/chat.js
+++ controllers/chat.js
@@ -4,23 +4,38 @@ module.exports = function(io) {
   chatNsp.on("connection", socket => {
     console.log("new chat connection");
 
-    const user1 = socket.handshake.query.user1;
-    const user2 = socket.handshake.query.user2;
-
-    let roomName = user1;
-
-    if (user1 < user2) {
-      roomName = roomName + user2;
-    } else {
-      roomName = user2 + roomName;
-    }
-
-    console.log(roomName);
+    let roomName = createChatRoomName(
+      socket.handshake.query.user1,
+      socket.handshake.query.user2
+    );
 
     socket.join(roomName);
+
+    socket.on("new message", message => {
+      console.log(message);
+
+      const roomName = createChatRoomName(
+        message.from.username,
+        message.to.username
+      );
+
+      socket.broadcast.to(roomName).emit("new message", message);
+    });
 
     socket.on("disconnect", () => {
       console.log("user left");
     });
   });
 };
+
+function createChatRoomName(username1, username2) {
+  let roomName = username1;
+
+  if (username1 < username2) {
+    roomName = roomName + username2;
+  } else {
+    roomName = username2 + roomName;
+  }
+
+  return roomName;
+}
